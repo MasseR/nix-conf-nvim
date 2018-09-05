a1ca7def941d5a46ba087967255f9fb33ab3d08e
Mats Rauhala <mats.rauhala@iki.fi>
2018-09-05 10:14:54 +0300

    Add gvim font
    Change-Id: Ic43c6dd5d6930a6e37cdf8792bb508e6cfe1f34b

--- vim.nix
+++ vim.nix
@@ -2,12 +2,21 @@
 
 let
   customizations = pkgs.callPackage ./customization.nix {};
+  gvimRC = ''
+    if(has('gui_running'))
+      set guioptions=
+      set guifont=Iosevka\ Term\ 11
+    endif
+  '';
 
 in
 
 pkgs.vim_configurable.customize {
   name = "vim";
-  vimrcConfig.customRC = customizations.customRC;
+  vimrcConfig.customRC = ''
+    ${customizations.customRC}
+
+    ${gvimRC}
+    '';
   vimrcConfig.packages.myVimPackage = customizations.plugins;
-  # extraPython3Packages = [ tasklib ];
 }

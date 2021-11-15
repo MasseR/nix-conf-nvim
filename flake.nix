a178d1429e7cbb75b70a5394f723c070f3d4d0b4
Mats Rauhala <mats.rauhala@iki.fi>
2021-11-15 21:24:43 +0200

    Flake support

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    # easy-dhall-nix = { url = "github:justinwoo/easy-dhall-nix"; flake = false; };
+  };
+
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-linux.myVim = nixpkgs.legacyPackages.x86_64-linux.callPackage ./vim.nix {};
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.myVim;
+    apps.x86_64-linux.myVim = {
+      type = "app";
+      program = "${self.packages.x86_64-linux.myVim}/bin/vim";
+    };
+    defaultApp.x86_64-linux = self.apps.x86_64-linux.myVim;
+
+    devShell.x86_64-linux = with nixpkgs.legacyPackages.x86_64-linux; mkShell {
+      buildInputs = [
+        dhall
+      ];
+    };
+
+  };
+}

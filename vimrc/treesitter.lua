9f98080aaaba8024901f41d836d0ab2c884b8cd3
Mats Rauhala <mats.rauhala@iki.fi>
2026-01-15 08:50:30 +0200

    Disable experimental indents

--- vimrc/treesitter.lua
+++ vimrc/treesitter.lua
@@ -16,13 +16,14 @@ vim.api.nvim_create_autocmd('FileType', {
     'gitignore',
     'mermaid',
     'elixir',
-    'rust'
+    'rust',
+    'lua'
   },
   callback = function()
     vim.treesitter.start()
     vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
     vim.wo[0][0].foldmethod = 'expr'
-    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    -- vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
   end,
 })
 
